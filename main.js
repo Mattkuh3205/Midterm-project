03af16103c706a9b0498ee5a07bfc4200c738c6d
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-15 15:45:24 -0400

    Most recent MT project

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log("hello world");
+console.log("I am "+20+" years old");
+console.log(`I am ${25} 
+
+
+years old.`);
+
+const a=5;
+const b=5;
+
+console.log(a==b);
+
+const person={
+    firstName: "first",
+    lastName: "last"
+}
+
+console.log(person.firstName);
+
+
+if(true){
+     y= 20;
+    console.log(y);
+}
+
+if(true){
+let x =10;
+
+console.log(x);
+console.log(y);
+}
+
+
+
+
+
