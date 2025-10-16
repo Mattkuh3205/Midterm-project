715a81ec3bfc31d14f8c30eefe52d68011a1efdf
Matthew Kuhns <mattkuh32@gmail.com>
2025-10-16 17:27:42 -0400

    Content Folder

--- data/content.js
+++ data/content.js
@@ -1,11 +1,10 @@
 //this is for content
 //create a JavaScript object representing data for your Web page’s main content
 const data = {
-    sections: {
         title: "title",
         main: "main",
 website: function(){
-    return `${this.title, this.main}`
+    return `${this.title} ${this.main}`
 }
 
         
@@ -22,5 +21,3 @@ website: function(){
 
 
 
-
-}
